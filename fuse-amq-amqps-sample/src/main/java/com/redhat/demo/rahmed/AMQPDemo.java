e80e86bf052775392a0dd2318f1273c776e677bf
Raif Ahmed <rahmed@redhat.com>
2021-03-18 16:29:28 +0200

    Update

--- fuse-amq-amqps-sample/src/main/java/com/redhat/demo/rahmed/AMQPDemo.java
+++ fuse-amq-amqps-sample/src/main/java/com/redhat/demo/rahmed/AMQPDemo.java
@@ -15,7 +15,7 @@ public class AMQPDemo extends RouteBuilder {
 
 	private String sourceName;
 	private String destinationName;
-	
+
 
 	public String getDestinationName() {
 		return destinationName;
@@ -38,20 +38,19 @@ public class AMQPDemo extends RouteBuilder {
 	public void configure() {
 
 		// Using Recipient List pattern: https://access.redhat.com/documentation/en-us/red_hat_fuse/7.8/html-single/apache_camel_development_guide/index#MsgRout-RecipientList
-		
+
 		from(getSourceName())
 		.routeId("route-from-queue-consumer").streamCaching().tracing()
 			.log("Recieved Message ${body} from Address " + getSourceName())
-		.log("Sending Message to both Queues " + getDestinationName())
 		.marshal().gzip() // use gzip to compress the message text
 		.setExchangePattern(ExchangePattern.InOnly)
 		.recipientList()
 			.constant(getDestinationName())
 			.parallelProcessing()
 			.log(String.format("Calling ${in.header.%s}", Exchange.RECIPIENT_LIST_ENDPOINT))
-			   
+
 		.end();
-		
+
 
 	}
 }
