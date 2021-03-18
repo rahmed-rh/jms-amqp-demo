f9cc32bd36076066e01cf5db966b90ea0e2efe96
Raif Ahmed <rahmed@redhat.com>
2021-03-18 16:35:01 +0200

    Update

--- jms-amqp-demo/src/main/java/com/redhat/rahmed/jms/Usecase4.java
+++ jms-amqp-demo/src/main/java/com/redhat/rahmed/jms/Usecase4.java
@@ -6,41 +6,25 @@ import java.util.concurrent.Executors;
 import org.apache.qpid.jms.JmsConnectionFactory;
 
 public class Usecase4 {
-	
-    public static void executeUseCase(String zoneAUrl,String zoneBUrl)
-    {
-		final int consumerCount=1;
-		final int producerCount=1;
-        JmsConnectionFactory connectionFactory1 = new JmsConnectionFactory();
-		connectionFactory1.setRemoteURI(
-			String.format("%s?%s", zoneAUrl, "failover.reconnectDelay=2000&failover.maxReconnectAttempts=-1&failover.warnAfterReconnectAttempts=10&failover.startupMaxReconnectAttempts=3"));
+
+	public static void executeUseCase(String zoneAUrl, String zoneBUrl) {
+		final int consumerCount = 1;
+		final int producerCount = 1;
+		JmsConnectionFactory connectionFactory1 = new JmsConnectionFactory();
+		connectionFactory1.setRemoteURI(String.format("%s?%s", zoneAUrl,
+				"failover.reconnectDelay=2000&failover.maxReconnectAttempts=-1&failover.warnAfterReconnectAttempts=10&failover.startupMaxReconnectAttempts=3"));
 		connectionFactory1.setUsername("admin@amq-interconnect-edge");
 		connectionFactory1.setPassword("admin");
-		
-		JmsConnectionFactory connectionFactory2 = new JmsConnectionFactory();
-		connectionFactory2.setRemoteURI(
-			String.format("%s?%s", zoneBUrl, "failover.reconnectDelay=2000&failover.maxReconnectAttempts=-1&failover.warnAfterReconnectAttempts=10&failover.startupMaxReconnectAttempts=3"));
-		connectionFactory2.setUsername("admin@amq-interconnect-edge");
-		connectionFactory2.setPassword("admin");
 
-				
-		String usecase4ProducerQueueName="telemetry.cortex.train.window.applicatienaam1";
-		String usecase4ConsumerQueueName="rh.usecase.4.train";
+		String usecase4ProducerQueueName = "telemetry.cortex.train.window.applicatienaam1";
 
-		JMSProducer usecase4QueueProducer = new JMSProducer(connectionFactory1, usecase4ProducerQueueName,false);
-		JMSQueueConsumer usecase4QueueConsumer = new JMSQueueConsumer(connectionFactory2,usecase4ConsumerQueueName,"Consumer-1");
+		JMSProducer usecase4QueueProducer = new JMSProducer(connectionFactory1, usecase4ProducerQueueName, false);
 
-		Executor usecase4QueueExecutor = Executors.newFixedThreadPool(consumerCount+producerCount);
-
+		Executor usecase4QueueExecutor = Executors.newFixedThreadPool(consumerCount + producerCount);
 
 		// Generate Producers on any dummy queue, it should be auto-created
-		for (int i=0;i<producerCount;i++)
+		for (int i = 0; i < producerCount; i++)
 			usecase4QueueExecutor.execute(usecase4QueueProducer);
-			
-		// Generate Consumers on the previously defined dummy queue
-		// for (int i=0;i<consumerCount;i++)
-			// usecase4QueueExecutor.execute(usecase4QueueConsumer);
-		
 
 	}
-} 
+}
